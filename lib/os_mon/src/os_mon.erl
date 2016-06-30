91a63f57a436f33554296d051346f41fb8428da4
Anders Nygren <anders.nygren@gmail.com>
2016-06-30 15:58:25 -0500

    MB-19708: Change restart values for os_mon
    Under heavy CPU oad on windows os_mon_sysinfo can timeout.
    Causing crashes in mem_sup and disk_sup, every 2 seconds, leading
    to os_mon termination.

--- lib/os_mon/src/os_mon.erl
+++ lib/os_mon/src/os_mon.erl
@@ -1,19 +1,19 @@
-%% 
+%%
 %% %CopyrightBegin%
-%% 
+%%
 %% Copyright Ericsson AB 1996-2013. All Rights Reserved.
-%% 
+%%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
 %% retrieved online at http://www.erlang.org/.
-%% 
+%%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
-%% 
+%%
 %% %CopyrightEnd%
 %%
 -module(os_mon).
@@ -112,7 +112,7 @@ stop(_) ->
 init([]) ->
     SupFlags = case os:type() of
 		   {win32, _} ->
-		       {one_for_one, 5, 3600};
+		       {one_for_one, 8, 5};
 		   _ ->
 		       {one_for_one, 4, 3600}
 	       end,
